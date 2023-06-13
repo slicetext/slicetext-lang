9c783d534f365d1cf3558d6e061feb0174e05ac5
Tgames <theokozak@gmail.com>
2023-06-13 06:21:47 -0600

    small changes

--- parse.cs
+++ parse.cs
@@ -6,6 +6,7 @@ namespace Parse
         private int i=0;
         private int line=0;
         private TokenType lookahead;
+        private List<string> ast= new List<string>();
         public void parse(List<TokenType> Tokens,List<object> Types)
         {
             for (i=0;i<Tokens.Count;i++)
@@ -19,7 +20,30 @@ namespace Parse
                 {
                     line++;
                 }
+                if(n_eat(new List<TokenType>{TokenType.NUMBER},Tokens[i]))
+                {
+                    ast.Add(Tokens[i].ToString());
+                    i++;
+                    while(n_eat(new List<TokenType>{TokenType.NUMBER,TokenType.PLUS},Tokens[i]))
+                    {
+                        if(Tokens[i]==TokenType.PLUS)
+                        {
+                        ast.Add(Tokens[i].ToString());
+                        }
+                        else
+                        {
+                            if(Types[i]!=null)
+                            {
+                                #pragma warning disable CS8604
+                                ast.Add(Types[i]?.ToString());
+                                #pragma warning restore CS8604
+                            }
+                        }
+                        i++;
+                    }
+                }
             }
+            Console.WriteLine(string.Join("",ast));
         }
         bool eat(List<TokenType> expected,TokenType lookahead)
         {
